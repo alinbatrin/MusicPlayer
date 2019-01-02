55c73503ad42063e0768bdbae80aaed4282a2082
alinbatrin <ssdd_wanted@yahoo.com>
2019-01-02 18:23:22 +0200

    Initial commit

---
+++ MusicPlayer/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace MusicPlayer
+{
+	public partial class MainPage : ContentPage
+	{
+        ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+        public MainPage()
+		{
+			InitializeComponent();
+            //Player load my default song.
+            player.Load("M1.mp3");
+        }
+
+        
+        void Play_Button_Clicked(object sender, EventArgs e)
+        {
+            //Play my default song.
+            player.Play();
+        }
+
+        void Pause_Button_Clicked(object sender, EventArgs e)
+        {
+            //Pause my default song.
+            player.Pause();
+        }
+    }
+}
